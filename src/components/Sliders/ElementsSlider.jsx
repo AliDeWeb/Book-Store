d433fa2c4a999c43f7a3065ab7ec6da1bf9caffd
Ali Moradi <Alimoradi0business@gmail.com>
2024-03-05 12:24:09 -0300

    main categories component added to main page

---
+++ src/components/Sliders/ElementsSlider.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+//? Css
+import "./ElementsSlider.css";
+
+//? Router
+import { NavLink } from "react-router-dom";
+
+export default function ElementsSlider(props) {
+  return (
+    <div className="py-2">
+      <div className="container">
+        <div className="w-full overflow-auto m-auto flex items-center justify-start gap-2 md:gap-10 child:child:text-white child:child:py-2 child:child:px-4 child:child:rounded-lg hover:child:child:bg-[#ffffff1f] child:child:transition-all child:child:text-sm child:child:sm:text-base child:child:inline-block">
+          {props.elemItems.map((el) => (
+            <div key={el.id}>
+              <NavLink
+                className={(status) =>
+                  `${status.isActive && "active-category"}`
+                }
+                to={el.path}
+              >
+                {el.title}
+              </NavLink>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useId } from "react";
 
 //? Components
 import Header from "../../components/Header/Header";
+import ElementsSlider from "../../components/Sliders/ElementsSlider";
 
 export default function Main() {
-  return <Header />;
+  //? States
+  const [mainCategories, setMainCategories] = useState([
+    { id: useId(), title: `All`, path: "/" },
+    { id: useId(), title: `Free`, path: "/free" },
+    { id: useId(), title: `Premium`, path: "/premium" },
+    { id: useId(), title: `Printed Books`, path: "/printed-book" },
+    { id: useId(), title: `Audio Books`, path: "/audio-books" },
+    { id: useId(), title: `Kids`, path: "/kids" },
+  ]);
+
+  return (
+    <>
+      <Header />
+      {/* Site Main Categories */}
+      <ElementsSlider elemItems={mainCategories} />
+    </>
+  );
 }
