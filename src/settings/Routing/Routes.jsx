2e42cf7f613b119c7cb88a6195d316dedf1237a1
Ali Moradi <Alimoradi0business@gmail.com>
2024-03-07 03:12:52 -0300

    some bugs fixed

--- src/settings/Routing/Routes.jsx
+++ src/settings/Routing/Routes.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
+import { BrowserRouter } from "react-router-dom";
+
 //? Elements
 import Main from "../../pages/main/Main";
 
 export default function Router() {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
